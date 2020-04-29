4a1eef935f1f6acbaa704d5c7248c039b11b234c
shahzeb1998 <64551696+shahzeb1998@users.noreply.github.com>
2020-04-30 03:49:06 +0500

    Update and rename zeroboy.py to Free.py

--- Free.py
+++ Free.py
@@ -49,16 +49,16 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•Shah - Zeb - KHAN ------•◈•
+|__\~~) •||•Mamash- khel ---------------•◈•
+|__(-----\  •◈•------Free-4U--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
+\033[1;96mAuthor  \033[1;93m: \033[1;Shahzeb Khan 
+\033[1;96mInstagram \033[1;93m: \033[1;Flow me
+\033[1;96mFacebook  \033[1;93m: \033[1; shahzeb1920
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
 \033[1;91m======================================="""
 
@@ -87,8 +87,8 @@ print  """\033[1;91m=======================================
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "free"
+CorrectPassword = "free"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://m.youtube.com/Free4UTechnical1998?uid=ZG3voUW1v3cO23k0Wyc0Hg')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://m.youtube.com/Free4UTechnical1998?uid=ZG3voUW1v3cO23k0Wyc0Hg')
 
 def login():
 	os.system('clear')
