e609f62e930ba151ce0b7dbd84376d9c6c57b76d
shahzeb1998 <64551696+shahzeb1998@users.noreply.github.com>
2020-04-30 04:08:56 +0500

    Update Free.py

--- Free.py
+++ Free.py
@@ -59,7 +59,7 @@ logo = """ -----------------------------•◈•
 \033[1;96mAuthor  \033[1;93m: \033[1;Shahzeb Khan 
 \033[1;96mInstagram \033[1;93m: \033[1;Flow me
 \033[1;96mFacebook  \033[1;93m: \033[1; shahzeb1920
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/free
 \033[1;91m======================================="""
 
 def tik():
@@ -80,10 +80,10 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92 shahzeb1998
+\033[1;96mInstagram \033[1;93m: \033[1;92mflow me
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Shahzeb1920
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/free
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
